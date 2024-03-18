a1dab358d71bc817afdec123f798b30b02a69a12
Dudeman062 <dudeman062@gmail.com>
2024-03-18 19:47:37 +0100

    Initial backend commit

---
+++ backend/src/main/java/com/example/syncmeet/SyncMeetApplication.java
@@ -0,0 +1,13 @@
+package com.example.syncmeet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyncmeetappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyncmeetappApplication.class, args);
+	}
+
+}
